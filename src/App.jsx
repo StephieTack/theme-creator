f1d64fd384d6a7b06d91759ae8336d0a86dde747
Stephanie Tack <stephierathjen@gmx.de>
2024-08-19 09:56:54 +0200

    adding id via nanoid-generator

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,25 @@
+import { useId, useState } from "react";
 import "./App.css";
 import ColorComponent from "./Components/Color/Color";
 import { initialColors } from "./lib/colors";
+import ColorForm from "./Components/ColorForm/ColorForm";
+import { nanoid } from "nanoid";
 
 function App() {
+  const [colors, setColors] = useState(initialColors);
+
+  function handleAddColor(newColor) {
+    const newColorWithId = { ...newColor, id: nanoid() };
+
+    setColors([newColorWithId, ...colors]);
+  }
+
   return (
     <>
       <h1>Theme Creator</h1>
-      <ul>
-        {initialColors.map((color) => (
+      <ColorForm onSubmitColor={handleAddColor} />
+      <ul className="color-list">
+        {colors.map((color) => (
           <li key={color.id}>
             <ColorComponent color={color} />
           </li>
